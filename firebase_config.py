9c8e8b5bac338edb371713699f5b0168962bdf62
Maggy-Morris <mag012805@gmail.com>
2023-12-03 21:46:35 +0200

    #adding firebase Config

---
+++ firebase_config.py
@@ -0,0 +1,10 @@
+firebaseConfig = {
+  "apiKey": "AIzaSyAGU1uOD4K3Aje2UqYbW6HrHQGe98aVXV0",
+  "authDomain": "shsh-3fec7.firebaseapp.com",
+  "databaseURL": "https://shsh-3fec7-default-rtdb.firebaseio.com",
+  "projectId": "shsh-3fec7",
+  "storageBucket": "shsh-3fec7.appspot.com",
+  "messagingSenderId": "52920915309",
+  "appId": "1:52920915309:web:a6205594f46e0eeac2b664",
+  "measurementId": "G-P1YYSZ8SZ2"
+}
